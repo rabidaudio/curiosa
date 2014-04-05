5f6aace52eb2c57cd56cd06955448b61ffa42cd5
Charles Julian Knight <charles@rabidaudio.com>
2014-04-05 05:13:15 -0400

    Created https certs

--- main.js
+++ main.js
@@ -1,42 +1,71 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var https = require('https');
+var fs = require('fs');
+
+
+var sslkey = fs.readFileSync('certs/ssl-key.pem');
+var sslcert = fs.readFileSync('certs/ssl-cert.pem');
+
+var options = {
+    key: sslkey,
+    cert: sslcert
+};
 
 var app = express();
 app.use(express.bodyParser());
 mongoose.connect('mongodb://localhost/test');
 
 
+var UserModel = require('models/user');
+var TagModel = require('models/tag');
+
+
 function err_handler(err){
 	if(err) throw err;
 }
 
-var tagSchema = mongoose.Schema({
-	data: String,
-});
 
-var userSchema = mongoose.Schema({
-	hash_id: {type: String, lowercase: true},
-	last_access: {type: Date, default: Date.now},
-	last_ip: String,
-});
+/*Methods (api.curiosadb.com/img/IMAGE-ID/)
+{	user:uuid, secret:secret	} <- required for all queries
+{	raters:int (default:all), tags:int (default: top 20) } <- optional for get queries
+
+	GET: 		get user's ratings and tags
+	POST/PUT: 	submit user's ratings and tags
+	DELETE:  	delete user's ratings and tags
+
+	/img/IMAGE-ID/rating
+	GET, POST/PUT, DELETE
+
+	/img/IMAGE-ID/tags
+	GET, POST (add tags), PUT(replace tags), DELETE
+
+	/img/IMAGE-ID/tags/:id
+	GET, POST/PUT, DELETE
+
+	READ ONLY (community)
+	/img/IMAGE-ID/all
+	/img/IMAGE-ID/all/rating
+	/img/IMAGE-ID/all/tags
+	/img/IMAGE-ID/all/tags/:id
+*/
 
 var imgSchema = mongoose.Schema({
 	hash_id: {type: String, lowercase: true},
 	user: [ userSchema ],
-	rating: {type:Number, default: 0},
+	rating: {type:Number, default: null},
 	tags: [ tagSchema ],
 });
 
 
 var Img = mongoose.model('Img', imgSchema);
-var User = mongoose.model('User', userSchema);
-var Tag = mongoose.model('Tag', tagSchema);
 
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-	var u = new User({
+	express.createServer(options);
+	/*var u = new User({
 		hash_id: "jules",
 		last_ip: "1234",
 	});
@@ -47,8 +76,8 @@ db.once('open', function() {
 		hash_id: "test1234",
 		user: u,
 		tags: t,
-	});
-	u.save(function(err){
+	});*/
+	/*u.save(function(err){
 		if(err) throw err;
 		t[0].save();
 		t[1].save(function(err){
@@ -65,5 +94,5 @@ db.once('open', function() {
 				});
 			});
 		});
-	});
+	});*/
 });
